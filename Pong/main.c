b786c08705f6b28c07b8036ea84a57c80b2c212b
Kaji Tetsushi <kajitetsushi@outlook.com>
2022-02-12 14:01:26 +0800

    Initial commit : Xcode

---
+++ Pong/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pong
+//
+//  Created by Kaji Tetsushi on 2/12/22.
+//  Copyright © 2022 Kaji Tetsushi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
