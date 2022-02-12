872ce313471c72264f86040a82f99752ffbb3d94
Kaji Tetsushi <kajitetsushi@outlook.com>
2022-02-12 15:46:26 +0800

    refactor(main): remove Hello World boilerplate

--- Pong/main.c
+++ Pong/main.c
@@ -6,10 +6,6 @@
 //  Copyright © 2022 Kaji Tetsushi. All rights reserved.
 //
 
-#include <stdio.h>
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
